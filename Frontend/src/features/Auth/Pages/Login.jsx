eb6a700101de0088ca04481c4a2cc0c397c55976
amankhan790 <amankassar790@gmail.com>
2026-06-10 00:37:54 +0530

    Authentication form ui

--- Frontend/src/features/Auth/Pages/Login.jsx
+++ Frontend/src/features/Auth/Pages/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import "../auth.style.scss";
+import { Link } from "react-router";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main className="login">
+      <h1>Login</h1>
+
+      <form
+        action="/login"
+        method="post"
+        className="login-form"
+        onSubmit={handleSubmit}
+      >
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            placeholder="Enter password"
+          />
+        </div>
+        <button type="submit" className="button primary-button">
+          Login
+        </button>
+      </form>
+      <p>
+        Don't have an account? <Link to={"/register"}>Register</Link>
+      </p>
+    </main>
+  );
+};
+
+export default Login;
--- Frontend/src/features/Auth/Pages/Register.jsx
+++ Frontend/src/features/Auth/Pages/Register.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main className="login">
+      <h1>Register</h1>
+
+      <form
+        action="/register"
+        method="post"
+        className="login-form"
+        onSubmit={handleSubmit}
+      >
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            placeholder="Enter username"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            placeholder="Enter password"
+          />
+        </div>
+        <button type="submit" className="button primary-button">
+          Login
+        </button>
+      </form>
+      <p>
+        Already have an account? <Link to={"/login"}>Login</Link>
+      </p>
+    </main>
+  );
 };
 
 export default Register;
